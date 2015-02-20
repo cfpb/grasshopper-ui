19ff7ae9eb836788489968a02dbe9a4b7486da70
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-02-20 00:40:27 -0500

    Removing much of the app skeleton for development. Will reintroduce as required after protoyping

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,9 @@
-console.log('\'Allo \'Allo!');
+document.addEventListener('DOMContentLoaded',function(){
+  'use strict';
+  var L = window.L;
+  L.mapbox.accessToken = 'pk.eyJ1IjoiY2ZwYiIsImEiOiJodmtiSk5zIn0.VkCynzmVYcLBxbyHzlvaQw';
+  var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([37.1, -127.58], 9);
+  console.log(map);
+});
 
