411b018cc8da20adf2563e7538adee9880617086
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-02-23 17:25:27 -0500

    callback for loading

--- src/scripts/loadLate.js
+++ src/scripts/loadLate.js
@@ -4,16 +4,19 @@ module.exports = function(){
   //Beware; appendChild doesn't work in all cases in ie6.
   var attachPoint = d.getElementsByTagName('script')[0].parentNode;
 
-  function script(url){
+  function script(url, cb){
     var s = d.createElement('script'); 
     s.src = url;
+    s.onload = cb;
     return attachPoint.appendChild(s);
   }
 
-  function css(url){
+  function css(url, cb){
     var l = d.createElement('link');
     l.ref = 'stylesheet';
     l.href = url;
+    //TODO img hack for css onload
+    l.onload = cb;
     return attachPoint.appendChild(l);
   }
 
