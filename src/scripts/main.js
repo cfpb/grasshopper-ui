ff73f006470cdded34bfac25a1e54cabc3ccc32b
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-02-24 11:50:28 -0500

    Reorg for map init

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,19 +8,26 @@ var subHub = subAndRun();
 
 var mapboxJS ='https://api.tiles.mapbox.com/mapbox.js/v2.1.5/mapbox.js';
 var mapboxCSS='https://api.tiles.mapbox.com/mapbox.js/v2.1.5/mapbox.css';
-L.mapbox.accessToken = 'pk.eyJ1IjoiY2ZwYiIsImEiOiJodmtiSk5zIn0.VkCynzmVYcLBxbyHzlvaQw';
-var map = L.mapbox.map('map', 'mapbox.streets').setView([38, -122], 10);
-var features = L.mapbox.featureLayer(null).addTo(map);
 
+
+function initMap(){
+  var L = window.L;
+  var mapDiv = document.getElementById('map');
+  L.mapbox.accessToken = 'pk.eyJ1IjoiY2ZwYiIsImEiOiJodmtiSk5zIn0.VkCynzmVYcLBxbyHzlvaQw';
+  var map = L.mapbox.map(mapDiv, 'mapbox.streets').setView([38, -122], 10);
+  var features = L.mapbox.featureLayer(null).addTo(map);
+  mapDiv.style.opacity=1;
+}
 
 
 function flipCoords(arr){
   return [arr[1],arr[0]];
 }
 
+
 function process(err, result){
   if(err){
-    result = randomBayPoint(); 
+    console.error(err);
   }
   var json = JSON.parse(result);
   subHub.run(json);
@@ -29,15 +36,24 @@ function process(err, result){
 
 subHub.subscribe(function(result){
   console.log(result);
-})
+});
+
 subHub.subscribe(function(result){
   features.setGeoJSON(result);
   map.panTo(flipCoords(result.coordinates));
 });
 
+
+var firstLoad = 1;
 var inp = document.getElementById('inp');
 inp.addEventListener('keydown',function(e){
   if(e.keyCode === 13){
+
+    if(firstLoad){
+      initMap();
+      firstLoad=0;
+    }
+
     var query = mapboxQuery(this.value);
     this.value = '';
     grass.geocode(query, process);
