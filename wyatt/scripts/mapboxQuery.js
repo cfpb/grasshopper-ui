fcd3caf9c69f504140172a0cd16c6454247e6492
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-03-10 15:30:34 -0400

    Streamline structure

--- wyatt/scripts/mapboxQuery.js
+++
@@ -1,6 +0,0 @@
-var prefix = 'http://api.tiles.mapbox.com/v4/geocode/mapbox.places/';
-var suffix = '.json?access_token=pk.eyJ1IjoiY2ZwYiIsImEiOiJodmtiSk5zIn0.VkCynzmVYcLBxbyHzlvaQw';
-
-module.exports = function(addr){
-  return prefix + encodeURIComponent(addr) + suffix;   
-}
