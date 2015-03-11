f0dc914abb298b366fa55a22bb987d54c315ab80
Andrew <awolfe76@gmail.com>
2015-03-11 16:16:39 -0400

    grunt working with a simple htmlmin task as default

---
+++ grunt/htmlmin.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dist: {
+        options: {
+            removeComments: true,
+            collapseWhitespace: true
+        },
+        files: {
+            'dist/index.html': 'index.html'
+        }
+    }
+}
