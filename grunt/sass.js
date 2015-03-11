3062f13c653b28629e4283b32af99d739e24aadf
Andrew <awolfe76@gmail.com>
2015-03-11 18:36:34 -0400

    changed sass to expanded css (default) and now use cssmin to create .min.css file

--- grunt/sass.js
+++ grunt/sass.js
@@ -2,7 +2,6 @@ module.exports = {
     dist: {
         options: {
             sourceMap: true,
-            outputStyle: 'compressed'
         },
         files: {
             'dist/css/main.css': 'scss/grasshopper.scss'
