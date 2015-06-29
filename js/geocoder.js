74203838d1ddd4fa2105b86819cef4b402988da5
Andrew <awolfe76@gmail.com>
2015-06-29 09:56:03 -0400

    add function to set id

--- js/geocoder.js
+++ js/geocoder.js
@@ -5,44 +5,43 @@ var lat,
     featureID;
 var features = [];
 
-function _setID(data) {
+function _setID(coordinates) {
+    lat = coordinates[1];
+    lon = coordinates[0];
+    latID = lat.toString().replace('.', '').replace('-', '');
+    lonID = lon.toString().replace('.', '').replace('-', '');
+    featureID = latID + lonID;
+
+    return featureID;
+}
+
+function _setProperties(data) {
     // check for status
     if (data.addressPointsService.status === 'ADDRESS_NOT_FOUND' && data.censusService.status === 'ADDRESS_NOT_FOUND') {
         features.push('No results found');
+    } else {
+        $.each(data.addressPointsService.features, function (i, result) {
+            // set the id
+            result.properties.id = _setID(result.geometry.coordinates);
+
+            // add the service, needed to output address correctly
+            result.properties.service = 'point';
+
+            // add to features array
+            features.push(result);
+        });
+
+        $.each(data.censusService.features, function (i, result) {
+            // set the id
+            result.properties.id = _setID(result.geometry.coordinates);
+
+            // add the service, needed to output address correctly
+            result.properties.service = 'census';
+
+            // add to features array
+            features.push(result);
+        });
     }
-    $.each(data.addressPointsService.features, function (i, result) {
-        // set the id
-        lat = result.geometry.coordinates[1];
-        lon = result.geometry.coordinates[0];
-        latID = lat.toString().replace('.', '').replace('-', '');
-        lonID = lon.toString().replace('.', '').replace('-', '');
-        featureID = latID + lonID;
-        result.properties.id = featureID;
-
-        // add the service
-        // needed to output address correctly
-        result.properties.service = 'point';
-
-        // add to features array
-        features.push(result);
-    });
-
-    $.each(data.censusService.features, function (i, result) {
-        // set the id
-        lat = result.geometry.coordinates[1];
-        lon = result.geometry.coordinates[0];
-        latID = lat.toString().replace('.', '').replace('-', '');
-        lonID = lon.toString().replace('.', '').replace('-', '');
-        featureID = latID + lonID;
-        result.properties.id = featureID;
-
-        // add the service
-        // needed to output address correctly
-        result.properties.service = 'census';
-
-        // add to features array
-        features.push(result);
-    });
 
     // return the new array
     return features;
@@ -52,14 +51,14 @@ module.exports = function(address) {
     features = [];
     var geodata;
     var newadd = address.replace(/ /g, '+');
-    
+
     $.ajax({
         url: '/api/geocoder/geocode/' + newadd,
         method: "GET",
         dataType: "json",
         async: false
     }).done(function(data) {
-        geodata = _setID(data);
+        geodata = _setProperties(data);
     }).error(function(request, status, error) {
         geodata = request.status;
     });
